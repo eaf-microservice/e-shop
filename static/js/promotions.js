4d21e812a2fbe763f3321c75e89a8be726c08773
eaf-microservice <EAF.microservice@gmail.com>
2026-02-05 17:48:17 +0100

    Separate products

--- static/js/promotions.js
+++ static/js/promotions.js
@@ -6,7 +6,7 @@ const promoProductsDB = {
             price: '300.00',
             original_price: '499.99',
             codeBar: '1234567890123',
-            image: 'static/images/products/product1.avif',
+            image: 'static/images/products/promotions/product1.avif',
             description: 'High-performance synthetic engine oil for all vehicles.',
             type: '10W40'
         },
@@ -16,7 +16,7 @@ const promoProductsDB = {
             price: '350.00',
             original_price: '549.99',
             codeBar: '9876543210987',
-            image: 'static/images/products/product2.avif',
+            image: 'static/images/products/promotions/product2.avif',
             description: 'High-performance synthetic engine oil for all vehicles.',
             type: 'Fluid'
         }
